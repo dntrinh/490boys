ef7d2865997193b0628c81edecc3cb19b0f675c5
Richmond Tran <richmondtran8@gmail.com>
2022-01-15 15:04:09 -0700

    Project setup.

---
+++ frontend/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+
+  return (
+    <View>
+      <Text>490Boys</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+});
+
+export default App;
